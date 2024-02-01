7dd2f89914c35886dc4e9efd1f8504f4bacff41d
alexcaneteantolinez <alxcaant@gmail.com>
2024-02-01 10:41:22 +0100

    Estructura Programa de AlexaderCA

--- PracticaCV_AlexaderCA/Program.cs
+++ PracticaCV_AlexaderCA/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //CONSTANTES
+
+            //VARIABLES
+
+            //ENTRADA
+
+            //PROCESS
+
+            //SALIDA
+
+            //Estructura de Programa de
         }
     }
 }
