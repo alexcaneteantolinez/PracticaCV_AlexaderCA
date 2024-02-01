e10e8527e7c7c4604476c351a00fa0754b152c6e
alexcaneteantolinez <alxcaant@gmail.com>
2024-02-01 10:54:13 +0100

    Creacion de Clase AlexandercA

---
+++ PracticaCV_AlexaderCA/AlexanderCA.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaCV_AlexaderCA
+{
+    internal class AlexanderCA
+    {
+        //CONSTANTES
+
+        //VARIABLES
+
+        //ENTRADA
+
+        //PROCESOS
+
+        //SALIDA
+    }
+}
